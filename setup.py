6287a5e7b31a7250f83499d8b9f4d22ae3e4a37e
rajandevkota98 <r.devkota.98@gmail.com>
2023-07-26 14:31:05 +0545

    first

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='search_engine_training',
+    author='Rajan Devkota',
+    version = '0.0.0.1',
+    author = 'Rajan Devkota',
+    author_email='r.devkota.98@gmail.com',
+    packages= find_packages()
+
+)
